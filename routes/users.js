4d2242cb730c1296f43d3f1fb1d2625596d4e7f4
Aaron Chen Desktop <chenaaron3@gmail.com>
2020-08-02 11:07:34 -0700

    google finance

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,8 @@ var yahooFinance = require('yahoo-finance');
 /* GET users listing. */
 router.get('/', async function (req, res, next) {
     yahooFinance.historical({
-        symbol: 'AAPL',
-        from: '2012-01-01',
+        symbol: 'AAN',
+        from: '1982-01-01',
         to: '2020-8-1',
         period: 'd'  // 'd' (daily), 'w' (weekly), 'm' (monthly), 'v' (dividends only)
     }, function (err, quotes) {
